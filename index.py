ac0b3fc46cc968809a838961a950e4e59b0f270e
Dima <dima.yerosh@gmail.com>
2021-03-28 19:38:39 +0200

    Added dock configuration (dockerfile) and env variables to run in production

--- index.py
+++ index.py
@@ -68,6 +68,10 @@ def page_not_found(e) -> 'html':
 
 
 
-
 if __name__ == '__main__':
-    app.run(debug = True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(
+            host = '0.0.0.0',
+            port = port,
+            debug = False
+            )
